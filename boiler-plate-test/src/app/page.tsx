18c34c99ead96da19c4456d55dbdf0cc4c0569ac
Seb <bustosospinat@gmail.com>
2024-11-25 08:46:42 -0600

    layout change in page.tsx

--- boiler-plate-test/src/app/page.tsx
+++ boiler-plate-test/src/app/page.tsx
@@ -8,10 +8,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+      <div>Header</div>
+      <main className="flex flex-col gap-8 row-[2] items-center w-full">
         <Hero />
+        <div className="">Testimonials</div>
+        <div className="">Services</div>
       </main>
+      <div>Footer</div>
     </div>
   )
 }
