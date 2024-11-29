72526004036d2f2fee2604839e21e3093242c63f
Seb <bustosospinat@gmail.com>
2024-11-29 17:58:16 -0600

    config

--- boiler-plate-test/next.config.js
+++ boiler-plate-test/next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.vercel.app',
+      },
+    ],
+  },
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
