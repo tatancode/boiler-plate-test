0eb68aebaf486b956cac533b21821835a35c51d6
Seb <bustosospinat@gmail.com>
2024-11-29 17:45:47 -0600

    please

--- boiler-plate-test/next.config.js
+++ boiler-plate-test/next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['boiler-plate-test.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.vercel.app',
+      },
+    ],
   },
   env: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
