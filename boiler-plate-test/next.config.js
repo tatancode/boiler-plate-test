fe462063ea833c38813b197d7db4a3a4ad1317b9
Seb <bustosospinat@gmail.com>
2024-11-29 18:00:37 -0600

    please work

--- boiler-plate-test/next.config.js
+++ boiler-plate-test/next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.vercel.app',
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
