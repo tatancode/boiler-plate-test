f59bab464fe6695b3fc0ad93c89b27aa4a2997d3
Seb <bustosospinat@gmail.com>
2024-11-29 18:06:44 -0600

    add fix

--- boiler-plate-test/next.config.js
+++ boiler-plate-test/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['boiler-plate-test.vercel.app'],
+  },
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+}
 
-export default nextConfig
+module.exports = nextConfig
